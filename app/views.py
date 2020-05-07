aeaa7d7aba164521513106a81a45ae06c74c5a45
TERRYYYY <kariuki.terry11@gmail.com>
2020-05-07 19:41:22 +0300

    adding quotes url to display random quotes

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from flask import render_template
+from app import app
+
+# Views
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title = "Hadithi-Blog"
+    return render_template('index.html', title=title)
