7c536228c6d0c341712ce5452adb8a327bc98396
TERRYYYY <kariuki.terry11@gmail.com>
2020-05-09 19:32:17 +0300

    creating a one to many relationship

--- app/models.py
+++ app/models.py
@@ -1,23 +1,60 @@
-class Blog:
+from . import db
 
-    all_blogs = []
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    blog_id = db.Column(db.Integer,db.ForeignKey('blogs.id'))
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Blog(db.Model):
+    __tablename__ = 'blogs'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+    def __repr__(self):
+        return f'User {self.name}'
 
 
-    def save_blogs(self):
-        Blog.all_blogs.append(self)
 
 
-    @classmethod
-    def clear_blogs(cls):
-        Blog.all_blogs.clear()
+
+
+
+
+
+
+
+
+
+
+
+
+# class Blog:
+
+#     all_blogs = []
+
+
+#     def save_blogs(self):
+#         Blog.all_blogs.append(self)
+
+
+#     @classmethod
+#     def clear_blogs(cls):
+#         Blog.all_blogs.clear()
     
-    @classmethod
-    def get_blogs(cls,id):
+#     @classmethod
+#     def get_blogs(cls,id):
 
-        response = []
+#         response = []
 
-        for blog in cls.all_blogs:
-            if blog.id == id:
-                response.append(blog)
+#         for blog in cls.all_blogs:
+#             if blog.id == id:
+#                 response.append(blog)
 
-        return response
+#         return response
+
--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
+from app.models import User,Blog
 
 # Creating app instance
 app = create_app('development')
@@ -7,5 +8,8 @@ app = create_app('development')
 manager = Manager(app)
 manager.add_command('server',Server)
 
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User, Blog = Blog )
 if __name__ == '__main__':
     manager.run()
