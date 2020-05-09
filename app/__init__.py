3ba3895cfdfe332f13cb6647ab639965797e138d
TERRYYYY <kariuki.terry11@gmail.com>
2020-05-09 19:52:29 +0300

    using Flask-Login as an extension that helps us manage the user authentication system

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,15 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -15,6 +21,7 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,8 @@ from flask import render_template,request,redirect,url_for
 from . import main
 from ..request import get_quote
 from .forms import BlogForm
-from ..models import Blog 
+from ..models import Blog
+from flask_login import login_required
 
 @main.route('/')
 def index():
@@ -12,3 +13,7 @@ def index():
     title = 'HadithiBlog'
 
     return render_template('index.html', title = title)
+
+@main.route('/blog', methods = ['GET','POST'])
+@login_required
+def new_blog():
