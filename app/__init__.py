5b27151c9e2ca38f1c8ba7dfca2cf2f93a86b3ab
TERRYYYY <kariuki.terry11@gmail.com>
2020-05-09 17:49:04 +0300

    creating and registering blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,24 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from .config import DevConfig
+from config import config_options
 
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
+bootstrap = Bootstrap()
 
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
+def create_app(config_name):
+    app = Flask(__name__)
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
+
+    
 from app import views
 from app import error
--- config.py
+++ config.py
@@ -1,8 +1,11 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
-    pass
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 
 
@@ -25,3 +28,8 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
