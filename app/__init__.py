62fe05a79c48496a1678afd153c237e0111a8750
TERRYYYY <kariuki.terry11@gmail.com>
2020-05-09 17:21:50 +0300

    adding an error page

--- app/__init__.py
+++ app/__init__.py
@@ -13,3 +13,4 @@ app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
 
 from app import views
+from app import error
--- app/views.py
+++ app/views.py
@@ -10,5 +10,15 @@ def index():
     View root page function that returns the index page and its data
     '''
     popular_quote = get_quote()
-    title = "Hadithi-Blog"
+    title = "HadithiBlog"
     return render_template('index.html', title=title ,popular_quote=popular_quote)
+
+@app.route('/blog/<int:id>')
+def blog(id):
+
+    '''
+    View blog page function that returns the blog details page and its data
+    '''
+    blog = get_quote(id)
+
+    return render_template('blog.html',blog = blog)
