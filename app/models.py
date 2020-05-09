6f88bb3e605df3519c0511b2af05f6a0cc88a721
TERRYYYY <kariuki.terry11@gmail.com>
2020-05-09 19:57:11 +0300

    registering new users

--- app/models.py
+++ app/models.py
@@ -1,10 +1,14 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
     blog_id = db.Column(db.Integer,db.ForeignKey('blogs.id'))
     pass_secure = db.Column(db.String(255))
 
@@ -33,6 +37,9 @@ class Blog(db.Model):
     def __repr__(self):
         return f'User {self.name}'
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 
