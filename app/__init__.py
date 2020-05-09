c25047c236d1b0f9584e11f2dd60a259b0b28234
TERRYYYY <kariuki.terry11@gmail.com>
2020-05-09 19:48:21 +0300

    securing passwords

--- app/__init__.py
+++ app/__init__.py
@@ -20,6 +20,9 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
     # setting config
     # from .request import configure_request
     # configure_request(app)
--- app/models.py
+++ app/models.py
@@ -8,6 +8,18 @@ class User(db.Model):
     blog_id = db.Column(db.Integer,db.ForeignKey('blogs.id'))
     pass_secure = db.Column(db.String(255))
 
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
     def __repr__(self):
         return f'User {self.username}'
 
