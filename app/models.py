2562fd4a4ee6b468c10fca86a70bc53d1d93c475
TERRYYYY <kariuki.terry11@gmail.com>
2020-05-09 19:41:58 +0300

    using the flask-migrate extension to create database migrations

--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     blog_id = db.Column(db.Integer,db.ForeignKey('blogs.id'))
+    pass_secure = db.Column(db.String(255))
 
     def __repr__(self):
         return f'User {self.username}'
--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
+from  flask_migrate import Migrate, MigrateCommand
 from app.models import User,Blog
 
 # Creating app instance
@@ -7,6 +8,8 @@ app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
