3a02d436d7ce6bbf7336df8a8d19e1b6496d5f37
TERRYYYY <kariuki.terry11@gmail.com>
2020-05-12 12:01:48 +0300

    deploy to heroku

--- manage.py
+++ manage.py
@@ -3,17 +3,27 @@ from flask_script import Manager,Server
 from  flask_migrate import Migrate, MigrateCommand
 from app.models import User,Blog
 
-# Creating app instance
-# app = create_app('development')
-app = create_app('production')
+app = create_app('development')
+
 
 manager = Manager(app)
-manager.add_command('server',Server)
-migrate = Migrate(app,db)
-manager.add_command('db',MigrateCommand)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+manager.add_command('server', Server)
+
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User, Blog = Blog )
-if __name__ == '__main__':
+    return dict(app=app, db=db, User=User, Blog=Blog)
+
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+if __name__ == "__main__":
     manager.run()
+    
